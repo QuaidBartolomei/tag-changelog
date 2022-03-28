929449024a2aff28894e68f8422b6e8562b0e2ab
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-03-27 17:28:43 -0700

    build(dev): update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-  extends: ["eslint:recommended"],
-  plugins: ["prettier"],
-  env: {
-    es6: true,
-    browser: false,
-    node: true,
-    commonjs: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": ["error", { printWidth: 200 }],
-    "prefer-const": "error",
-    eqeqeq: "error",
-    "no-useless-return": "error",
-  },
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+        ,"prettier"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
 };
